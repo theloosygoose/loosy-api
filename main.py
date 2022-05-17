07b5637e4b27a2b0cf772096122d46d0fa7585ff
Gusti Henry <g.ramahenry2439@gmail.com>
2022-05-17 13:12:09 -0400

    printed the game logs of 100 games to a csv

--- main.py
+++ main.py
@@ -2,8 +2,6 @@
 # coding: utf-8
 
 # In[41]:
-
-
 import pandas as pd
 import requests
 import numpy as np
@@ -13,22 +11,31 @@ from nba_api.stats.static import teams
 from nba_api.stats.endpoints import leaguegamefinder
 from nba_api.stats.library.parameters import Season
 from nba_api.stats.library.parameters import SeasonType
+from nba_api.stats.endpoints import playbyplay
+
+custom_headers = {
+    'Host': 'stats.nba.com',
+    'Connection': 'keep-alive',
+    'Cache-Control': 'max-age=0',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'en-US,en;q=0.9',
+}
 
 
 # In[42]:
-
-
 nba_teams = teams.get_teams()
-nba_teams_df = pd.DataFrame(nba_teams, columns=['id', 'full_name', 'abbreviation', 'nickname', 'city', 'state', 'year_founded'])
+nba_teams_df = pd.DataFrame(nba_teams)
 nba_teams_id = nba_teams_df['id']
-
 
 # In[43]:
 #Parsing to get pbp data
 #Calling the teams function to get games ids that the team has played
 temp_03 = []
 for i in nba_teams_id:
-    temp = leaguegamefinder.LeagueGameFinder(team_id_nullable = [i], season_nullable=Season.default, season_type_nullable=SeasonType.regular)
+    temp = leaguegamefinder.LeagueGameFinder(team_id_nullable = [i], season_nullable=Season.default, season_type_nullable=SeasonType.regular) 
     temp_01 = temp.get_normalized_dict()
     temp_02 = temp_01['LeagueGameFinderResults']
 
@@ -36,10 +43,19 @@ for i in nba_teams_id:
         temp_03.append(temp_02[i]['GAME_ID'])
     #temp_03 += temp_02[['GAME_ID']]
     #time.sleep(0.600)
-
     
 print(temp_03)
-
-
-
 # In[44]
+
+df = pd.DataFrame()
+
+for i in temp_03:
+    print(i)
+    df = df.append(playbyplay.PlayByPlay(i).get_data_frames()[0])
+
+df
+# %%
+
+df_01 = playbyplay.PlayByPlay(temp_03[2]).get_data_frames()[0]
+df_01
+# %%
