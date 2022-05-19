2c28edaa53641b6c3dc3992aa942dcd3dabf69b7
Gusti Henry <g.ramahenry2439@gmail.com>
2022-05-19 19:28:06 -0400

    get 10 most recent tweets about luka

--- twitter_api/nba_sentiment.py
+++ twitter_api/nba_sentiment.py
@@ -1,2 +1,24 @@
+
+#%%
 import pandas as pd
 import numpy as np
+import tweepy
+import twitter_credentials
+
+#%%
+#Authorizations of Keys and Tokens for Twitter API Project
+auth = tweepy.OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET, twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
+
+api = tweepy.API(auth) 
+
+client = tweepy.Client(bearer_token=twitter_credentials.BEARER_TOKEN)
+
+# %%
+query = "luka OR luka doncic"
+
+response = client.search_recent_tweets(query=query, max_results=10)
+
+
+for tweet in response.data:
+    print(tweet.text)
+# %%
